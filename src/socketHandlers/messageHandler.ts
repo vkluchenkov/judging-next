3e48f86a29f6f2c236d14c1e7d998ec44bfcd0bb
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-05-01 16:36:26 +0200

    feat: strapi login

--- src/socketHandlers/messageHandler.ts
+++ src/socketHandlers/messageHandler.ts
@@ -2,6 +2,7 @@ import { Server as ServerIO } from 'socket.io';
 import { DefaultEventsMap } from '@socket.io/component-emitter';
 import { Socket } from 'socket.io';
 import { LoginPayload } from '../components/Login/types';
+import axios from 'axios';
 
 interface MessageHandlerProps {
   io: ServerIO<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
@@ -11,7 +12,14 @@ interface MessageHandlerProps {
 export const messageHandler = ({ io, socket }: MessageHandlerProps) => {
   socket.emit('hello', 'Welcome to competition socket.io server');
 
-  socket.on('login', (loginPayload: LoginPayload) => {
+  socket.on('login', async (loginPayload: LoginPayload) => {
     console.log(loginPayload);
+    await axios
+      .post('http://192.168.1.45:1337/api/auth/local/', {
+        identifier: loginPayload.username,
+        password: loginPayload.password,
+      })
+      .then((response) => console.log(response.data))
+      .catch((error) => console.log(error));
   });
 };

749b23096fe20f7063db8a460ffd4b19c4849896
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-05-01 12:41:02 +0200

    feat: basic socket.io setup

---
+++ pages/api/socket.ts
@@ -0,0 +1,28 @@
+import { NextApiResponseServerIO } from '@/src/types/socket';
+import { NextApiRequest } from 'next';
+import { Server as ServerIO } from 'socket.io';
+import { Server as NetServer } from 'http';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const SocketHandler = (req: NextApiRequest, res: NextApiResponseServerIO) => {
+  if (res.socket.server.io) {
+    console.log('Socket is already running');
+  } else {
+    console.log('Socket is initializing');
+    const httpServer: NetServer = res.socket.server as any;
+    const io = new ServerIO(httpServer);
+    res.socket.server.io = io;
+
+    io.on('connection', (socket) => {
+      socket.emit('hello', 'test message');
+    });
+  }
+  res.end();
+};
+
+export default SocketHandler;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
-import { LoginPayload } from "@/src/components/Login/types";
-import { Main } from "@/src/components/Main";
-import { Login } from "@/src/components/Login";
+import { useState, useEffect } from 'react';
+import { LoginPayload } from '@/src/components/Login/types';
+import { Main } from '@/src/components/Main';
+import { Login } from '@/src/components/Login';
+import io from 'socket.io-client';
 
 export default function Home() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  let socket;
 
   const handleLogout = () => setIsLoggedIn(false);
 
   const handleLogin = (loginPayload: LoginPayload) => {
     setIsLoggedIn(true);
+  };
+
+  useEffect(() => {
+    socketInitializer();
+  }, []);
+
+  const socketInitializer = async () => {
+    // We just call it because we don't need anything else out of it
+    await fetch('/api/socket');
+
+    socket = io();
+
+    socket.on('hello', (msg) => {
+      console.log(msg);
+    });
   };
 
   return isLoggedIn ? <Main /> : <Login onLogin={handleLogin} />;

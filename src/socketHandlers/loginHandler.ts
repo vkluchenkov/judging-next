7587b7e548df773ab022b2794407a5af6032e187
Vladimir Kluchenkov <vkluchenkov@gmail.com>
2023-05-01 17:19:36 +0200

    feat: strapi login

--- src/socketHandlers/loginHandler.ts
+++ src/socketHandlers/loginHandler.ts
@@ -2,6 +2,7 @@ import { LoginPayload } from '../components/Login/types';
 import { Server as ServerIO } from 'socket.io';
 import { DefaultEventsMap } from '@socket.io/component-emitter';
 import { Socket } from 'socket.io';
+import axios from 'axios';
 
 interface LoginHandlerProps {
   loginPayload: LoginPayload;
@@ -9,6 +10,22 @@ interface LoginHandlerProps {
   socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
 }
 
-export const LoginHandler = (args: LoginHandlerProps) => {
+export const LoginHandler = async (args: LoginHandlerProps) => {
   const { loginPayload, io, socket } = args;
+  const apiUrl = process.env.API_URL!;
+
+  const response = await axios.post(`${apiUrl}/auth/local/`, {
+    identifier: loginPayload.username,
+    password: loginPayload.password,
+  });
+
+  if (response) {
+    const token = response.data.jwt;
+    const user = await axios.get(`${apiUrl}/users/me?populate=*`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(user.data);
+  }
 };
--- src/socketHandlers/messageHandler.ts
+++ src/socketHandlers/messageHandler.ts
@@ -2,7 +2,7 @@ import { Server as ServerIO } from 'socket.io';
 import { DefaultEventsMap } from '@socket.io/component-emitter';
 import { Socket } from 'socket.io';
 import { LoginPayload } from '../components/Login/types';
-import axios from 'axios';
+import { LoginHandler } from './loginHandler';
 
 interface MessageHandlerProps {
   io: ServerIO<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
@@ -13,13 +13,10 @@ export const messageHandler = ({ io, socket }: MessageHandlerProps) => {
   socket.emit('hello', 'Welcome to competition socket.io server');
 
   socket.on('login', async (loginPayload: LoginPayload) => {
-    console.log(loginPayload);
-    await axios
-      .post('http://192.168.1.45:1337/api/auth/local/', {
-        identifier: loginPayload.username,
-        password: loginPayload.password,
-      })
-      .then((response) => console.log(response.data))
-      .catch((error) => console.log(error));
+    try {
+      await LoginHandler({ loginPayload, socket, io });
+    } catch (error) {
+      console.log(error);
+    }
   });
 };
